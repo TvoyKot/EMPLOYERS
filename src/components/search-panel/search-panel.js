dca7e2f5b6bfb683124276545cec2c8df19011ec
Alexey <a.lshe@yandex.by>
2025-09-09 04:56:33 +0300

    Made template of app

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import "./search-panel.css";
+
+const SearchPanel = () => {
+  return (
+    <input
+      className="form-control search-input"
+      placeholder="Найти сотрудника"
+      type="text"
+    />
+  );
+};
+
+export default SearchPanel;
